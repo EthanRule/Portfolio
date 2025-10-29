bebe9130c61cfe65b78710529755ea848f5086f7
EthanRule <ethan2002@live.com>
2025-10-29 16:02:52 -0700

    prismjs code editor

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import Header from "@/components/Header";
 import AboutMe from "@/components/AboutMe";
-import TechnicalSkills from "@/components/TechnicalSkills";
 import Academics from "@/components/Academics";
 import FeaturedProjects from "@/components/FeaturedProjects";
 import Footer from "@/components/Footer";
+import Toolkit from "@/components/Toolkit/TechnicalSkills";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
-      <Header />
+      <Header />{" "}
+      {/** maybe make this point to cool projects instead of garbage seeking links*/}
       <AboutMe />
-      <TechnicalSkills />
+      <Toolkit />
       <Academics />
       <FeaturedProjects />
       <Footer />
---
+++ portfolio/components/Toolkit/ToolImage.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Image from "next/image";
+
+interface ToolImageProps {
+  src: string;
+  alt: string;
+  shadowColor?: string;
+}
+export function ToolImage({
+  src,
+  alt,
+  shadowColor = "blue-400",
+}: ToolImageProps) {
+  const shadowClasses: Record<string, string> = {
+    "blue-400": "rounded-lg shadow-lg shadow-blue-400",
+    "amber-400": "rounded-lg shadow-lg shadow-amber-400",
+    "purple-800": "rounded-lg shadow-lg shadow-purple-800",
+    white: "rounded-lg shadow-lg shadow-white",
+    "cyan-400": "rounded-lg shadow-lg shadow-cyan-400",
+    "cyan-700": "rounded-lg shadow-lg shadow-cyan-700",
+  };
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={64}
+      height={64}
+      className={shadowClasses[shadowColor] || shadowClasses["blue-400"]}
+    />
+  );
+}
