ac8129a6a5592a72e2039ab438fb9d61c6b5437f
EthanRule <ethan2002@live.com>
2026-01-07 21:10:24 -0800

    tetris and about me update

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import Header from "@/components/Header";
+import AboutMe from "@/components/AboutMe";
 import Academics from "@/components/Academics";
 import FeaturedProjects from "@/components/FeaturedProjects";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
       <Header />{" "}
       {/** maybe make this point to cool projects instead of garbage seeking links*/}
+      <AboutMe />
       <FeaturedProjects />
       <Academics />
-      <Footer />
     </div>
   );
 }
